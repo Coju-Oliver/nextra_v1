e0478d7abd8c44f844d349deaf174770b0669ff2
Coju-Oliver <oliver.hees@conversion-junkies.de>
2025-12-02 13:45:44 +0100

    docs: Add initial Nextra documentation pages, configure navigation, and update theme settings.

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Conversion Junkies Dokumentation</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/conversionjunkies',
   },
-  chat: {
+  /*chat: {
     link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  },*/
+  docsRepositoryBase: 'https://github.com/conversionjunkies/nextra_v1',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Conversion Junkies Dokumentation',
   },
 }
 
